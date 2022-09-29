9a5e5564ef805f033fc93eeb9ff18b16f343a2b4
JohnCard <107288337+JohnCard@users.noreply.github.com>
2022-09-28 22:40:32 -0500

    update - 10:40 p. m.

--- reto_semana_13.py
+++ reto_semana_13.py
@@ -1,12 +1,37 @@
 import matplotlib.pyplot as plt
 
+def mostrar_menu():
+  'Esta función se encargará de mostrale al usuario el menu de opciones que puede digitar'
+  
+  print('''
+  Digite la nueva acción a realizar :
+  ● Agregar un nuevo alumno (1).
+
+  ● Ver los alumnos y las calificaciones (2).
+
+  ● Salir (S)
+          ''')
+  
+def advertencia():
+  'Esta función se encarga de indicarle de nuevo al usuario cuales opciones estan disponibles para digitar'
+  
+  print('''
+  Usted debe seleccionar una de las opciones siguientes de a cuerdo a su necesidad:
+  ● Agregar un nuevo alumno (1).
+
+  ● Ver los alumnos y las calificaciones (2).
+
+  ● Salir (S).
+''')
+
 # inicializamos con esta variable yaque será la que contenga los caracteres permitidos en los nombres 
 # que se digiten en el programa
 abc = 'abcdefghijklmñnopqrstuvwxyzABCDEFGHIJKLMÑNOPQRSTUVWXYZ '
 
 def recorrer_cadena(param):
   '''
-  esta función recibirá un param, el cual hará referencia a una cadena digitada por el usuario, lo que hará
+  esta función recibirá un param, el cual hará referencia a una cadena d
+  igitada por el usuario, lo que hará
   esta función será recorrer esa cadena (param), con el cilo for que tiene para revisar cada una de sus 
   caracteres y asegurarse de que ninguno sea inexistente en la variable abc, yaque de ser asi, se le 
   aumentará un uno a la variable cont, que contará como tal cuantas variables no permitidas hay, aunque
@@ -43,7 +68,7 @@ calificaciones = []
 #  de cada alumno en esta lista👽😁🕸
 promedios = []
 
-# esta variable (cont_dos - linea 22) la inicializaremos en 0, y para cuando inicie el programa de abajo, ira 
+# esta variable (cont_dos) la inicializaremos en 0, y para cuando inicie el programa de abajo, ira 
 # sumandose de uno en uno cada que el usuario escoja la opción "1" (cada que answer se iguale a 1), ya que si 
 # escoje la opción 1, por ende dará a entender que quiere registrar un nuevo nombre y calificaciones nuevas de 
 # un alumno, entonces, con eso queremos dar a entender que por ende esta variable tendria por logica la 
@@ -51,19 +76,12 @@ promedios = []
 # terminar de digitar nombres o calificaciones, pero, para que?, bueno, sucede que de acuerdo a como se le 
 # valla aumentando un uno a esta variable, será justo el nuevo valor del cont_dos lo que tomaremos de 
 # referencia para indicarle la nueva posición ala que ingresaremos la nueva lista de notas del nuevo alumno en 
-# la lista calificaciones de la linea 20
+# la lista calificaciones
 cont_dos = 0
 answer = input('\t Imprima su primer respuesta (S/1/2): ')
 while True:
   if(answer != 'S' and answer != 's' and answer != '1' and answer != '2'):
-    print('''
-  Usted debe seleccionar una de las opciones siguientes de a cuerdo a su necesidad:
-  ● Agregar un nuevo alumno (1).
-
-  ● Ver los alumnos y las calificaciones (2).
-
-  ● Salir (S).
-''')
+    advertencia()
     answer = input('Digite nuevamente su respuesta (S/1/2): ')
   elif(answer == '1'):
     # a partir de aqui hasta la linea 58 va la lógica que permitirá al usuario registrar un nuevo nombre y las
@@ -95,21 +113,15 @@ while True:
       nota_sumatoria += nota
     promedio = nota_sumatoria/len(calificaciones[cont_dos-1])
     promedios.append(promedio)
-    print('''
-  Digite la nueva acción a realizar :
-  ● Agregar un nuevo alumno (1).
-
-  ● Ver los alumnos y las calificaciones (2).
-
-  ● Salir (S)
-          ''')
+    mostrar_menu()
     answer = input('Digite su respuesta: ')
-    # a partir de aqui si la variable legase a cambiar su valor por un 2, segun los requisitos, nos pide que 
+    
+  elif(answer == '2'):
+    # a partir de aqui si la variable llegase a cambiar su valor por un 2, segun los requisitos, nos pide que 
     # escribamos la logica para que de esa forma se le muestre al usuario los nombres de los alumnos y los 
     # promedios de cada uno, y esta parte del codigo esta hecha de tal manera que que conforme avance el 
     # sistema, mostrará los nombres y los promedios que se lleve registrados hasta ahora de las variables de 
     # tipo lista (lista_nombres y calificaciones) declaradas en las lineas 19 y 20
-  elif(answer == '2'):
     alumno = -1
     print('Nombres y promedios de alumnos registrados hasta ahora: ')
     for nombre in lista_nombres:
@@ -121,14 +133,7 @@ while True:
       print(f'''
       Nombre del alumno {alumno+1}: {nombre}
       Promedio de {nombre}: {promedios[alumno]: .3f}''')
-    print('''
-  Digite la nueva acción a realizar :
-  ● Agregar un nuevo alumno (1).
-
-  ● Ver los alumnos y las calificaciones (2).
-
-  ● Salir (S)
-          ''')
+    mostrar_menu()
     answer = input('Digite su respuesta: ')
   elif(answer == 's' or answer == 'S'):
     answer_dos = input('Esta seguro de que desea salir del programa (S/N)? ')
@@ -137,14 +142,7 @@ while True:
     if(answer_dos == 's' or answer_dos == 'S'):
       break
     else:
-      print('''
-  Digite la nueva acción a realizar :
-  ● Agregar un nuevo alumno (1).
-
-  ● Ver los alumnos y las calificaciones (2).
-
-  ● Salir (S)
-          ''')
+      mostrar_menu()
       answer = input('Digite su respuesta: ')
       
 # Una pequeña practica extra 😁😎👽🧐🤠🤓 con matplotlib.pyplot con los nombres de cada alumno y sus 
